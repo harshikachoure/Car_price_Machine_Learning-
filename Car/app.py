e36f01d770e3a1e160674340b65dc879a722264f
harshikachoure <choureharshika.3011@gmail.com>
2025-09-17 18:22:50 +0530

    Update app.py

--- Car/app.py
+++ Car/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle as pk 
 import streamlit as st
 
-model = pk.load(open('model.pkl','rb'))
+model = pk.load(open('Car/model.pkl','rb'))
 
 st.header('Car Price Prediction ML Model')
 
@@ -47,5 +47,6 @@ if st.button("Predict"):
                           ,inplace=True)
 
     car_price = model.predict(input_data_model)
+
 
     st.markdown('Car Price is going to be '+ str(car_price[0]))
